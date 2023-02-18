78a6f788245a3c981ee855b23a375aa7be537b8c
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-02-18 15:57:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from robyn import Robyn, jsonify
-from controllers import all_books, new_book, book_by_id
+from controllers import all_books, new_book, book_by_id, delete_book, update_book
 import json
 from dotenv import load_dotenv
 import os
@@ -56,15 +56,53 @@ async def books(request):
 @app.get("/book/:id")
 async def get_book(request):
     id = request['params']['id']
-    book_id = int(id)
     print(id)
+
 
     book = book_by_id(id)
 
     if book == []:
         return {"status_code":404, "body": "Book not Found", "type": "text"}
+    else:
+        return book   
+        
+@app.put("/book/:id")
+async def update(request):
+    id = request['params']['id']
+    print(id)
+
+    body = bytearray(request['body']).decode("utf-8")
+    json_body = json.loads(body)
+
+    title = json_body['title']
+    author = json_body['author']
+    pages_num = json_body['pages_num']
+    review = json_body['review']
+    print(title)
+
+    book_id = book_by_id(id)
+    print(book_id)
+
+    if book_id == []:
+        return {"status_code":404, "body": "Book not Found", "type": "text"}
     else:    
+        book = update_book(title, book_id)
+        print(book)
         return book
+
+
+
+@app.delete("/book/:id")
+async def delete(request):
+    id = request['params']['id']
+    print(id)
+
+    book = delete_book(id)
+
+    if book != "Book deleted":
+        return {"status_code":404, "body": "Book not Found", "type": "text"}
+    else:    
+        return book    
 
 
 
