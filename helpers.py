c91f9e9fc3f8a6c74b5ac2c20094e10c62c307e8
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-02-22 00:20:18 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -5,14 +5,11 @@ from datetime import datetime
 
 def to_dict(psycopg_object:tuple):
 
-
     book_dict = collections.OrderedDict()
     book_dict['id'] = psycopg_object[0]
     book_dict['title'] = psycopg_object[1]
     book_dict['author'] = psycopg_object[2]
-    book_dict['pages_num'] = psycopg_object[3]
-    book_dict['review'] = psycopg_object[4]
-    book_dict['datetime'] = psycopg_object[5].strftime("%m/%d/%Y")
+    book_dict['datetime'] = psycopg_object[3].strftime("%m/%d/%Y")
     return book_dict
 
 def list_dict(rows:list):
